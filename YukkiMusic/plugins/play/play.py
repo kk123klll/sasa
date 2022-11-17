4b5f95c958c9026e4d4610c4af2cde47085d5615
K-musicBot <118387239+K-musicBot@users.noreply.github.com>
2022-11-17 15:26:54 +0300

    Update play.py

--- YukkiMusic/plugins/play/play.py
+++ YukkiMusic/plugins/play/play.py
@@ -40,7 +40,7 @@ force_btn = InlineKeyboardMarkup(
     [
         [
             InlineKeyboardButton(   
-              text=f"• 𝗬𝗮𝗙𝗮 🦋", url=f"https://t.me/ssAee",)                        
+              text=f"- أضغط للأشتࢪاك", url=f"https://t.me/ssAee",)                        
         ],        
     ]
 )
@@ -50,7 +50,7 @@ async def check_is_joined(message):
         status = await app.get_chat_member("ssAee", userid)
         return True
     except Exception:
-        await message.reply_text("🥲︰ عذرا، عليك الانضمام الى قناة البوت أولاً :",reply_markup=force_btn,parse_mode="markdown",disable_web_page_preview=False)
+        await message.reply_text("💡 ¦ عذࢪاً ، أشترك في قناة البوت أولاً :",reply_markup=force_btn,parse_mode="markdown",disable_web_page_preview=False)
         return False
 
 # Command
