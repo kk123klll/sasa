68f922011dc89b97fb9b59de6f99050810b22487
K-Music777 <118857624+K-Music777@users.noreply.github.com>
2022-12-04 21:26:14 +0300

    Update start.py

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -104,7 +104,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
             InlineKeyboardButton(
                 text=_["S_B_5"],
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
-            )
+                )
                 text="• اضافة لقناة •",
                 url=f"https://t.me/{BOT_USERNAME}?startchannel=true",
             )
