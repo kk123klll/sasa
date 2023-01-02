4523bc8ebf130bface96fc3c93b68ed43666db1a
DevKassem <121644358+DevKassem@users.noreply.github.com>
2023-01-03 01:10:05 +0300

    Update devloper.py

--- YukkiMusic/plugins/play/devloper.py
+++ YukkiMusic/plugins/play/devloper.py
@@ -4,50 +4,23 @@ from strings import get_command
 from strings.filters import command
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
-from typing import Union
-from pyrogram.types import InlineKeyboardButton
-
-from config import GITHUB_REPO, SUPPORT_CHANNEL, SUPPORT_GROUP
-from YukkiMusic import app
-from config import BANNED_USERS, MUSIC_BOT_NAME
-
-import re
-import sys
-from os import getenv
-
-from dotenv import load_dotenv
-from pyrogram import filters
-
-load_dotenv()
-
-BOT_USERNAME = getenv("BOT_USERNAME")
-
-IMG_DEV1 = getenv("IMG_DEV1")
-
-OWNER = getenv("OWNER")
-
-OWNER_NAME = getenv("OWNER_NAME")
-
-OWNER_BIO = getenv("OWNER_BIO")
 
 
 @app.on_message(
-    command(["المطور"])
+    command(["المطور","المبرمج"])
     & filters.group
     & ~filters.edited
 )
 async def khalid(client: Client, message: Message):
-    await message.reply_photo(
-        photo=f"{IMG_DEV1}",
-       caption=f"""{OWNER_BIO}\n@{OWNER}""",
+    usr = await client.get_users()
+    name = usr.first_name
+    async for photo in client.iter_profile_photos(5946704196, limit=1):
+                    await message.reply_photo(photo.file_id,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        f"{OWNER_NAME}", url=f"https://t.me/{OWNER}")
-                ],[
-                    InlineKeyboardButton(
-                        ". اضافة الى مجموعة -", url=f"https://t.me/{BOT_USERNAME}?startgroup=true"),
+                        name, url=f"https://t.me/{OWNER}"),
                 ],
             ]
         ),
