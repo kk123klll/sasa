8b2da8c8a2a31fe3dd921948836dddd2c0ae4d66
DevKassem <121644358+DevKassem@users.noreply.github.com>
2023-01-03 01:44:57 +0300

    Update devloper.py

--- YukkiMusic/plugins/play/devloper.py
+++ YukkiMusic/plugins/play/devloper.py
@@ -5,14 +5,56 @@ from strings.filters import command
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
 
+def get_file_id(msg: Message):
 
+    if msg.media:
+
+        for message_type in (
+
+            "photo",
+
+            "animation",
+
+            "audio",
+
+            "document",
+
+            "video",
+
+            "video_note",
+
+            "voice",
+
+            # "contact",
+
+            # "dice",
+
+            # "poll",
+
+            # "location",
+
+            # "venue",
+
+            "sticker",
+
+        ):
+
+            obj = getattr(msg, message_type)
+
+            if obj:
+
+                setattr(obj, "message_type", message_type)
+
+                return obj
+
+            
 @app.on_message(
     command(["المطور","المبرمج"])
     & filters.group
     & ~filters.edited
 )
 async def khalid(client: Client, message: Message):
-    usr = await client.get_users(5946704196)
+    usr = await client.get_users()
     name = usr.first_name
     async for photo in client.iter_profile_photos(5946704196, limit=1):
                     await message.reply_photo(photo.file_id,
