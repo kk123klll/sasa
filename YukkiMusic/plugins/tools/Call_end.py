b5f79e881a58a59823f413a98a10b242f58fbd90
DevKassem <121644358+DevKassem@users.noreply.github.com>
2023-01-04 00:03:16 +0300

    Create Call_end.py

---
+++ YukkiMusic/plugins/tools/Call_end.py
@@ -0,0 +1,16 @@
+import asyncio
+from pyrogram import Client, filters
+from strings import get_command
+from strings.filters import command
+from pyrogram.types import Message, VoiceChatStarted, VoiceChatEnded
+from YukkiMusic import app
+
+@app.on_message(filters.voice_chat_started)
+async def babloo(client: Client, message: Message): 
+      Startt = "- صعدوا نسمع أغاني 🥲"
+      await message.reply_text(Startt)
+
+@app.on_message(filters.voice_chat_ended)
+async def bablo(client: Client, message: Message): 
+      Enddd = "- أصلاً مليت 🙂"
+      await message.reply_text(Enddd)
