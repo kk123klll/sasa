7cab3771a342c1034403c00bcdd777a87e73486d
DevKassem <121644358+DevKassem@users.noreply.github.com>
2023-01-03 12:53:18 +0300

    Update play.py

--- YukkiMusic/plugins/play/play.py
+++ YukkiMusic/plugins/play/play.py
@@ -83,7 +83,7 @@ async def play_commnd(
     url,
     fplay,
 ):
-    if not await check_is_joined(message):
+    if not await check_is_joined(message, client: Client):
         return
     mystic = await message.reply_text(
         _["play_2"].format(channel) if channel else _["play_1"]
