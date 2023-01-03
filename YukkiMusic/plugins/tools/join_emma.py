3555b9546d553a58c28d7047aec4c6424c598580
DevKassem <121644358+DevKassem@users.noreply.github.com>
2023-01-04 00:33:27 +0300

    Update join_emma.py

--- YukkiMusic/plugins/tools/join_emma.py
+++ YukkiMusic/plugins/tools/join_emma.py
@@ -19,7 +19,7 @@ async def must_join_channel(bot: Client, msg: Message):
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"⌯︙عذࢪاَ عزيزي ↫ {message.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة البـوت اولآ\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ",
+                    f"⌯︙عذࢪاَ عزيزي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة البـوت اولآ\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
                         [InlineKeyboardButton(u"Emma 𝟸𝟶𝟸𝟹 🎄", url=link)]
