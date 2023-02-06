e568a52183f7ccb811805de80c803484b0f43a80
xiipq <124061336+xiipq@users.noreply.github.com>
2023-02-06 13:49:50 +0300

    Update play.py

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -39,7 +39,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text=f"Emma 𝟸𝟶𝟸𝟹 🎄", url=f"https://t.me/sspaa",
+                text=f"برود .", url=f"https://t.me/zzSvv",
             )
         ],
     ]
