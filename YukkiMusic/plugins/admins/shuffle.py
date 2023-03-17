facd4ce1a07b0639d4e415d9c1ac80b64b6c0562
xiipq <124061336+xiipq@users.noreply.github.com>
2023-03-17 17:50:53 +0300

    Update shuffle.py

--- YukkiMusic/plugins/admins/shuffle.py
+++ YukkiMusic/plugins/admins/shuffle.py
@@ -27,6 +27,12 @@ SHUFFLE_COMMAND = get_command("SHUFFLE_COMMAND")
     & filters.group
     & ~filters.edited
     & ~BANNED_USERS
+) 
+@app.on_message(
+    filters.command("ترتيب")
+    & filters.group
+    & ~filters.edited
+    & ~BANNED_USERS
 )
 @AdminRightsCheck
 async def admins(Client, message: Message, _, chat_id):
