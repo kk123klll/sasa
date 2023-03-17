d15047dc0dd9d14f233aad8950a1fe5d0f8368c6
xiipq <124061336+xiipq@users.noreply.github.com>
2023-03-17 17:43:02 +0300

    Update resume.py

--- YukkiMusic/plugins/admins/resume.py
+++ YukkiMusic/plugins/admins/resume.py
@@ -26,6 +26,12 @@ RESUME_COMMAND = get_command("RESUME_COMMAND")
     & filters.group
     & ~filters.edited
     & ~BANNED_USERS
+) 
+@app.on_message(
+    filters.command("استئناف")
+    & filters.group
+    & ~filters.edited
+    & ~BANNED_USERS
 )
 @AdminRightsCheck
 async def resume_com(cli, message: Message, _, chat_id):
