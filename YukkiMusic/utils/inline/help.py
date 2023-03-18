3624354cf2cf93a7e5ca3357dbd7aa88c12c836f
xiipq <124061336+xiipq@users.noreply.github.com>
2023-03-18 19:29:12 +0300

    Update help.py

--- YukkiMusic/utils/inline/help.py
+++ YukkiMusic/utils/inline/help.py
@@ -37,25 +37,25 @@ def help_pannel(_, START: Union[bool, int] = None):
                     text=_["H_B_1"],
                     callback_data="help_callback hb1",
                 ),
+            ],
+            [
                 InlineKeyboardButton(
                     text=_["H_B_2"],
                     callback_data="help_callback hb2",
                 ),
-            ],
-            [
                 InlineKeyboardButton(
                     text=_["H_B_3"],
                     callback_data="help_callback hb3",
                 ),
-                InlineKeyboardButton(
-                    text=_["H_B_4"],
-                    callback_data="help_callback hb4",
-                ),
             ],
             [
                 InlineKeyboardButton(
+                    text=_["H_B_4"],
+                    callback_data="help_callback hb4",
+                    ),
+                InlineKeyboardButton(
                     text=_["H_B_6"],
-                    callback_data="help_callback hb5",
+                    callback_data="help_callback hb6",
                 ),
             ],
             mark,
