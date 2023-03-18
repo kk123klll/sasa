b531105c026e61a0d1fe8693fd94b4b399613238
xiipq <124061336+xiipq@users.noreply.github.com>
2023-03-18 17:09:20 +0300

    Update playy.py

--- YukkiMusic/plugins/play/playy.py
+++ YukkiMusic/plugins/play/playy.py
@@ -40,7 +40,7 @@ from YukkiMusic.utils.stream.stream import stream
 PLAY_COMMAND = get_command("PLAY_COMMAND")
 
 @app.on_message(
-    command(["قناة","تشغيل","شغل"])
+    command(["ليان شغلي","تشغيل","شغل"])
     & filters.channel
     & ~filters.edited
     & ~BANNED_USERS
