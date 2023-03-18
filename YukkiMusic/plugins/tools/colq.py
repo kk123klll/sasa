ec8c07d65585d51a102d356088dbc6d8f53a9fb8
xiipq <124061336+xiipq@users.noreply.github.com>
2023-03-18 19:05:33 +0300

    Update colq.py

--- YukkiMusic/plugins/tools/colq.py
+++ YukkiMusic/plugins/tools/colq.py
@@ -12,40 +12,40 @@ async def strcall(client, message):
     assistant = await group_assistant(Yukki,message.chat.id)
     try:
         await assistant.join_group_call(message.chat.id, AudioPiped("./assets/mody.mp3"), stream_type=StreamType().pulse_stream)
-        text="☣ الاعضاء المتواجدين في الاتصال :\n\n"
+        text="الاعضاء المتواجدين في الاتصال:⇊\n\n"
         participants = await assistant.get_participants(message.chat.id)
         k =0
         for participant in participants:
             info = participant
             if info.muted == False:
-                mut=f"٭{participant.volume}٭يتحدث ⚛٭"
+                mut=f"◉{participant.volume}◉ ♡ يتحدث ◉"
             else:
-                mut="٭ساكت ☯٭"
+                mut="◎ ♪ ساكت ◎"
             user = await client.get_users(participant.user_id)
             print(participant.user_id)
             k +=1
-            text +=f"{k}➤{user.mention}{mut}\n"
-        text += f"\nعددهم : {len(participants)}\n✔️"    
+            text +=f"{k}➲{user.mention}{mut}\n"
+        text += f"\nعددهم : {len(participants)}\n✓"    
         await message.reply(f"{text}")
         await asyncio.sleep(5)
         await assistant.leave_group_call(message.chat.id)
     except NoActiveGroupCall:
-        await message.reply(f"عمري الاتصال مش مفتوح اصلااا\n❌")
+        await message.reply(f"عمري الاتصال مش مفتوح اصلااا\n✗")
     except AlreadyJoinedError:
-        text="☣ الاعضاء المتواجدين في الاتصال :\n\n"
+        text="الاعضاء المتواجدين في الاتصال: ⇊\n\n"
         participants = await assistant.get_participants(message.chat.id)
         k =0
         for participant in participants:
             info = participant
             if info.muted == False:
-                mut=f"٭{participant.volume}٭يتحدث ⚛٭"
+                mut=f"٭{participant.volume}◉ ♡ يتحدث ◉"
             else:
-                mut="٭ساكت ☯٭"
+                mut="◎ ♪ ساكت ◎"
             user = await client.get_users(participant.user_id)
             print(participant.user_id)
             k +=1
-            text +=f"{k}➤{user.mention}{mut}\n"
-        text += f"\nعددهم : {len(participants)}\n✔️"
+            text +=f"{k}➲{user.mention}{mut}\n"
+        text += f"\nعددهم : {len(participants)}\n✓"
         await message.reply(f"{text}")
     except TelegramServerError:
-        await message.reply(f"ارسل الامر تاني في مشكله في سيرفر التلجرام\n❌")
+        await message.reply(f"ارسل الامر تاني في مشكله في سيرفر التلجرام\n✗")
