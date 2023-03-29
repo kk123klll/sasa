f159e8258aebecccd1e8c6ffa902c429e383a73a
coowl <129096669+coowl@users.noreply.github.com>
2023-03-30 01:22:47 +0300

    Update playy.py

--- YukkiMusic/plugins/play/playy.py
+++ YukkiMusic/plugins/play/playy.py
@@ -40,7 +40,7 @@ from YukkiMusic.utils.stream.stream import stream
 PLAY_COMMAND = get_command("PLAY_COMMAND")
 
 @app.on_message(
-    command(["ليان شغلي","تشغيل","شغل"])
+    command(["شغل","تشغيل"])
     & filters.channel
     & ~filters.edited
     & ~BANNED_USERS
