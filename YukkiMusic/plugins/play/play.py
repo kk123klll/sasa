c1f9747ccb98cbb76aca6f7ef07c51a6f8ea45ba
coowl <129096669+coowl@users.noreply.github.com>
2023-05-21 14:25:48 +0300

    Update play.py

--- YukkiMusic/plugins/play/play.py
+++ YukkiMusic/plugins/play/play.py
@@ -40,7 +40,7 @@ force_btn = InlineKeyboardMarkup(
     [
         [
             InlineKeyboardButton(   
-              text=f"اضغط للاشتراك", url=f"https://t.me/sx_hu",)                        
+              text=f"اضغط للاشتراك", url=f"https://t.me/llVYVY",)                        
         ],        
     ]
 )
@@ -48,7 +48,7 @@ async def check_is_joined(message, Message, client):
     try:
         userid = message.from_user.id
         user_name = message.from_user.first_name
-        status = await app.get_chat_member("sx_hu", userid)
+        status = await app.get_chat_member("llVYVY", userid)
         return True
     except Exception:
         await message.reply_text(f'⌯︙عذࢪاَ عزيزي ↫ {user_name} \n⌯︙عـليك الاشـتࢪاك في قنـاة البـوت اولآ\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ',reply_markup=force_btn,parse_mode="markdown",disable_web_page_preview=False)
