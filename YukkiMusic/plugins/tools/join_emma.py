a0c9ca9f264098fe6c90f89ea5bfb7110a696e2c
coowl <129096669+coowl@users.noreply.github.com>
2023-07-10 22:38:46 +0300

    Update join_emma.py

--- YukkiMusic/plugins/tools/join_emma.py
+++ YukkiMusic/plugins/tools/join_emma.py
@@ -6,27 +6,27 @@ from YukkiMusic import app
 
 @app.on_message(~filters.edited & filters.incoming & filters.private, group=-1)
 async def must_join_channel(bot: Client, msg: Message):
-    if not "https://t.me/llVYVY":  # Not compulsory
+    if not "https://t.me/ll0llld":  # Not compulsory
         return
     try:
         try:
-            await bot.get_chat_member("llVYVY", msg.from_user.id)
+            await bot.get_chat_member("ll0llld", msg.from_user.id)
         except UserNotParticipant:
-            if "https://t.me/llVYVY".isalpha():
-                link = "https://t.me/llVYVY"
+            if "https://t.me/ll0llld".isalpha():
+                link = "https://t.me/ll0llld"
             else:
-                chat_info = await bot.get_chat("llVYVY")
+                chat_info = await bot.get_chat("ll0llld")
                 link = chat_info.invite_link
             try:
                 await msg.reply(
                     f"⌯︙عذࢪاَ عزيزي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة البـوت اولآ\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton(u"╏𝗠𝗘𝗫𝗜𝗖𝗢 - سورس مكسيكو", url=link)]
+                        [InlineKeyboardButton(u"سورس الاعصار ⟠ ⦂", url=link)]
                     ])
                 )
                 await msg.stop_propagation()
             except ChatWriteForbidden:
                 pass
     except ChatAdminRequired:
-        print(f"عليك رفع البوت آدمن في القناة أولاً ؟؟ : @llVYVY !")
+        print(f"عليك رفع البوت آدمن في القناة أولاً ؟؟ : @ll0llld !")
